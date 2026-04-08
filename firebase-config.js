cd6d1e806b4db96f5bf7cd7a3731baf13c94cbfb
Luis-Niquen33 <U23255587@utp.edu.pe>
2026-04-08 01:31:53 -0500

    Configurar credenciales Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 globalThis.FIREBASE_CONFIG = {
-	apiKey: "",
-	authDomain: "",
-	databaseURL: "",
-	projectId: "",
-	storageBucket: "",
-	messagingSenderId: "",
-	appId: ""
+	apiKey: "AIzaSyCoMguYMSO715CBcZ_tLPtjJdbAHpIk6-c",
+	authDomain: "celulaieppce.firebaseapp.com",
+	databaseURL: "https://celulaieppce-default-rtdb.firebaseio.com",
+	projectId: "celulaieppce",
+	storageBucket: "celulaieppce.firebasestorage.app",
+	messagingSenderId: "25575669284",
+	appId: "1:25575669284:web:87435174c6ade4462fe2b1"
 };
